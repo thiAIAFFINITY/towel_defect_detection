128d9a5a8e15022fd800659f2fcba8b4dd50b109
duyaiaffinity <duy@aiaffinity.us>
2022-08-23 16:08:46 +0700

    modify directory

--- label_consistent.py
+++ label_consistent.py
@@ -1,17 +1,23 @@
 import os
 
-ROOT_DIR = "./cvat_input"
-ROOT_LABEL = "./cvat_input/MẪU"
+ROOT_DIR = "."
+ROOT_LABEL = "./MẪU"
+
+
 class LabelConsistent:
     def __init__(self, root_dir: str, root_label: str):
-        self.root_dir = root_dir 
-        self.root_label = root_label            
+        self.root_dir = root_dir
+        self.root_label = root_label
 
     def _create_class_for_each_product(self):
 
-        product_ids = [product_id for product_id in os.listdir(self.root_dir) if self.root_dir + "/" + product_id != self.root_label]
+        product_ids = [
+            product_id
+            for product_id in os.listdir(self.root_dir)
+            if self.root_dir + "/" + product_id != self.root_label
+            and os.path.isdir(self.root_dir + "/" + product_id)
+        ]
         # Using BFS for creating folder class label
-
         root_each_class = []
         root_each_class.append(self.root_label)
 
@@ -24,7 +30,14 @@ class LabelConsistent:
             for class_name in os.listdir(root_dir):
                 for product_id in product_ids:
                     new_root_dir = root_dir.replace(self.root_label, "")
-                    new_directory = self.root_dir + "/" + product_id + "/" + new_root_dir + "/" + class_name
+                    new_directory = (
+                        self.root_dir
+                        + "/"
+                        + product_id
+                        + new_root_dir
+                        + "/"
+                        + class_name
+                    )
                     if os.path.isdir(root_dir + "/" + class_name) == False:
                         break
 
@@ -34,6 +47,7 @@ class LabelConsistent:
                         print("Warning:", new_directory, "has been existed")
 
                 root_each_class.append(root_dir + "/" + class_name)
+
 
 obj = LabelConsistent(ROOT_DIR, ROOT_LABEL)
 obj._create_class_for_each_product()
